1e844b2927685b285186f092815efc85e368af51
Shabab <shababarshadbutt@outlook.com>
2022-06-17 19:26:49 +0500

    Update Code

---
+++ SmartSolutions.Mobile/SmartSolutions.Mobile/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSolutions.Mobile
+{
+    public class AppSettings
+    {
+        public static string BaseAddress { get; set; } = "http://192.168.99.19:63639";
+    }
+}
